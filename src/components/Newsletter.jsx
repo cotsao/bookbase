2dfe004a8410f42f0e277c4e9c46f0e14a7da6bc
cotsao <codytsao@gmail.com>
2021-09-22 01:56:26 -0700

    homepage styling continuation

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,17 +1,41 @@
 function Newsletter() {
   return (
-    <div className="newsletter-container">
-      <div className="newsletter-card">
-        <img
-          className="newsletter-img"
-          src="https://images.unsplash.com/photo-1523634921619-37ce98c1877f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"
-          alt=""
-        />
-        <div>
-          <h3 className="newsletter-title">STAY WITH US</h3>
-          <h6 className="newsletter-text">subscribe to our newsletter to get updates on new features</h6>
-          <input type="text" />
+    <div className="newsletter-wrapper">
+      <div className="newsletter-container"> 
+      <img
+        className="newsletter-img"
+        src="https://source.unsplash.com/collection/1246622/1600x900"
+        alt=""
+      />
+      <div className="newsletter-text-container">
+        <div className="newsletter-content-container">
+          <h3 className="newsletter-title med-font">stay with us</h3>
+          <h6 className="newsletter-text sml-font">
+            subscribe to our newsletter to get updates on new features
+          </h6>
+          <div className="newsletter-input">
+            <input type="text" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon icon-tabler icon-tabler-news"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="#2d373c"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M16 6h3a1 1 0 0 1 1 1v11a2 2 0 0 1 -4 0v-13a1 1 0 0 0 -1 -1h-10a1 1 0 0 0 -1 1v12a3 3 0 0 0 3 3h11" />
+              <line x1="8" y1="8" x2="12" y2="8" />
+              <line x1="8" y1="12" x2="12" y2="12" />
+              <line x1="8" y1="16" x2="12" y2="16" />
+            </svg>
+          </div>
         </div>
+      </div>
       </div>
     </div>
   );
